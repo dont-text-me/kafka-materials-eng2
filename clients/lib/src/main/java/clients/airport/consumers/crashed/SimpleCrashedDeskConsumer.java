437e9ef5b4f46c67e9def585df8a73be8593e245
beep-boop-boop <ivanbar3330@gmail.com>
2023-09-29 00:45:00 +0100

    implement crashed check ins counter (practical 2)

--- clients/lib/src/main/java/clients/airport/consumers/crashed/SimpleCrashedDeskConsumer.java
+++ clients/lib/src/main/java/clients/airport/consumers/crashed/SimpleCrashedDeskConsumer.java
@@ -1,11 +1,15 @@
 package clients.airport.consumers.crashed;
 
+import clients.airport.AirportProducer;
 import clients.airport.consumers.AbstractInteractiveShutdownConsumer;
 import clients.messages.MessageProducer;
+import java.time.Duration;
 import java.time.Instant;
-import java.util.Map;
-import java.util.Properties;
-import java.util.TreeMap;
+import java.util.*;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
 
 /** Consumer which will print out terminals that haven't a STATUS event in a while. */
 public class SimpleCrashedDeskConsumer extends AbstractInteractiveShutdownConsumer {
@@ -19,8 +23,33 @@ public class SimpleCrashedDeskConsumer extends AbstractInteractiveShutdownConsum
     props.put("enable.auto.commit", "true");
 
     Map<Integer, Instant> lastHeartbeat = new TreeMap<>();
+    Set<Integer> crashedMachines = new HashSet<>();
 
-    // TODO: exercise
+    try (KafkaConsumer<Integer, AirportProducer.TerminalInfo> consumer =
+        new KafkaConsumer<>(
+            props, new IntegerDeserializer(), new AirportProducer.TerminalInfoDeserializer())) {
+      consumer.subscribe(Collections.singleton(AirportProducer.TOPIC_STATUS));
+
+      while (!done) {
+        ConsumerRecords<Integer, AirportProducer.TerminalInfo> records =
+            consumer.poll(Duration.ofSeconds(1));
+        for (ConsumerRecord<Integer, AirportProducer.TerminalInfo> record : records) {
+          // update last timestamps
+          lastHeartbeat.put(record.key(), Instant.ofEpochMilli(record.timestamp()));
+        }
+
+        var currentTimestamp = Instant.now();
+
+        lastHeartbeat.forEach(
+            (recordKey, recordHeartbeat) -> {
+              if (Duration.between(recordHeartbeat, currentTimestamp).toSeconds() >= 12) {
+                System.out.printf("Machine with key %s has crashed!%n", recordKey);
+                crashedMachines.add(recordKey);
+              }
+            });
+        System.out.printf("%s machines crashed so far %n", crashedMachines.size());
+      }
+    }
   }
 
   public static void main(String[] args) {
